18b8585f4ac96148d922046fb1af0b6cfab1d7dd
cds803 <cds803@126.com>
2018-03-10 23:09:53 +0800

    add min-eslint

---
+++ packages/min-eslint/README.md
@@ -0,0 +1,15 @@
+# min eslint
+
+## 安装
+
+``` bash
+$ npm install @mindev/min-eslint --save-dev
+```
+
+## 配置`min.config.json`
+
+``` json
+{
+  "eslint": true
+}
+```
---
+++ packages/min-eslint/src/types/eslint.d.ts
@@ -0,0 +1,17 @@
+import eslint from 'eslint'
+
+// declare module 'eslint-friendly-formatter' {
+
+//   function formatter (): eslint.CLIEngine.Formatter
+
+//   export = formatter
+// }
+
+// declare module 'eslint' {
+//   export namespace CLIEngine {
+//     class Options {
+//       output: string | boolean
+//     }
+//   }
+// }
+
