66f906521e5c25daaa79440f5122ac754560b27c
cds803 <cds803@126.com>
2018-03-20 10:01:16 +0800

    优化less编译器

--- packages/min-compiler-less/src/index.ts
+++ packages/min-compiler-less/src/index.ts
@@ -1,25 +1,36 @@
-import less from 'less'
-import path from 'path'
+import * as path from 'path'
+import * as _ from 'lodash'
+import * as less from 'less'
 import { CompilerHelper } from '@mindev/min-core'
 
-export default async function (options: CompilerHelper.Options): Promise<string> {
-  let { filename, extend = {}, config: $config } = options
-  let { content = '' } = extend
-  let opath = path.parse(filename)
-  let p = Promise.resolve(content)
+import Compiler = CompilerHelper.Compiler
+import Options = CompilerHelper.Options
+import Result = CompilerHelper.Result
 
-  if (!content) {
+const compiler: Compiler = async (options: Options): Promise<Result> => {
+  let { filename, extend = {}, config: $config } = options
+  let { code = '' } = extend
+  let opath = path.parse(filename)
+  let p = Promise.resolve(options)
+
+  if (!code) {
     return p
   }
 
-  let config: Less.Options = Object.assign({}, $config, {
-    filename,
-    paths: [opath.dir]
-  })
-
   try {
-    let css = await less.render(content, config).then(result => result.css)
-    p = Promise.resolve(css)
+    let config: Less.Options = Object.assign({}, $config, {
+      filename,
+      paths: [opath.dir]
+    })
+    let result = await less.render(code, config)
+
+    _.merge(options, {
+      extend: {
+        code: result.css,
+        map: result.map,
+        imports: result.imports
+      }
+    })
   }
   catch (err) {
     p = Promise.reject(err)
@@ -27,3 +38,5 @@ export default async function (options: CompilerHelper.Options): Promise<string>
 
   return p
 }
+
+export default compiler
