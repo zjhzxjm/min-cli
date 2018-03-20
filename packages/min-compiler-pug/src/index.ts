1a969127dadd24dfd4a5adfe9cb43cb4bac768fc
cds803 <cds803@126.com>
2018-03-20 16:13:23 +0800

    min-complier-pug 增加sync方法

--- packages/min-compiler-pug/src/index.ts
+++ packages/min-compiler-pug/src/index.ts
@@ -1,21 +1,21 @@
-import pug from 'pug'
+import * as _ from 'lodash'
+import * as pug from 'pug'
 import { CompilerHelper } from '@mindev/min-core'
 
-export default function (options: CompilerHelper.Options): Promise<string> {
-  let { filename, extend = {}, config } = options
-  let { content = '' } = extend
-  let { data } = config
-  let p = Promise.resolve(content)
+import Compiler = CompilerHelper.Compiler
+import Options = CompilerHelper.Options
+import Result = CompilerHelper.Result
 
-  if (!content) {
-    return p
-  }
+const noop = (options: Options): Result => {
+  return options
+}
+
+const compiler: Compiler = (options: Options): Promise<Result> => {
+  let p = Promise.resolve(options)
+  let { sync = noop } = compiler
 
   try {
-    delete config.data
-    let templete = pug.compile(content, config)
-    let html = templete(data)
-    p = Promise.resolve(html)
+    sync(options)
   }
   catch (err) {
     p = Promise.reject(err)
@@ -23,3 +23,29 @@ export default function (options: CompilerHelper.Options): Promise<string> {
 
   return p
 }
+
+compiler.sync = (options: Options): Result => {
+  let {
+    extend: {
+      code = ''
+    } = {},
+    config = {}
+  } = options
+
+  if (!code) {
+    return options
+  }
+
+  let templete = pug.compile(code, _.omit(config, ['data']))
+  let html = templete(config.data)
+
+  _.merge(options, {
+    extend: {
+      code: html
+    }
+  })
+
+  return options
+}
+
+export default compiler
