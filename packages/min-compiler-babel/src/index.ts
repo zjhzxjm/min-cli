82d103aedbbedb82f98cb1a0d873c10e63ae36b0
cds803 <cds803@126.com>
2018-03-28 17:56:31 +0800

    min-compiler-babel 支持 .babelrc 配置

--- packages/min-compiler-babel/src/index.ts
+++ packages/min-compiler-babel/src/index.ts
@@ -32,9 +32,16 @@ compiler.sync = (options: Options): Result => {
     return options
   }
 
+  // 如果 config 为 false 只处理语法树转换
+  let babelrc = config === false ? false : true
+
+  if (!_.isPlainObject(config)) {
+    config = {}
+  }
+
   let result = transformFromAst(ast, code, {
     ast: false,
-    babelrc: false,
+    babelrc,
     ...config,
     filename
   })
